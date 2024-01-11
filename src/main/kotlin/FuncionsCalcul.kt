f9e8eb9a14ab92274d57bd61109dee37ae46719b
jacomar <jacomar304@hotmail.com>
2024-01-11 01:04:15 +0100

    Commit 4: Añadida la función 'calculantElCostDelConsum' al archivo 'FuncionsCalcul.kt' que nos permite calcular el coste de los litros consumidos.

--- src/main/kotlin/FuncionsCalcul.kt
+++ src/main/kotlin/FuncionsCalcul.kt
@@ -1,6 +1,26 @@
 fun emetreFactura() {
-    var litresConsumits = llegirLitresConsumits()
-    var unitatFamiliar = llegirTipusDeUnitatFamiliar()
-    var membresUnitatFamiliar = llegirMembresDeLaUnitatFamiliar()
-    var boSocial = llegirBoSocial()
+    // Llegim dades d'entrada
+    var litresConsumits:Float = llegirLitresConsumits()
+    var unitatFamiliar:Boolean = llegirTipusDeUnitatFamiliar()
+    var membresUnitatFamiliar:Int = if (unitatFamiliar) llegirMembresDeLaUnitatFamiliar()
+                                else 0
+    var boSocial:Boolean = llegirBoSocial()
+
+    // Inicialitzem variables
+    var quotaFixa:Float = if (boSocial) 3.0f
+                          else 6.0f
+    var descompteFNM:Float = if (unitatFamiliar && membresUnitatFamiliar >= 2) 0.1f * membresUnitatFamiliar
+                             else 0.0f
+    var descompteBoSocial:Float = 0.8f
+
+    //Calculem el cost del consum
+    var costDelsLitresConsumits:Float = calculantElCostDelConsum(litresConsumits)
+}
+
+fun calculantElCostDelConsum (plitresConsumits:Float): Float {
+    return when {
+        plitresConsumits < 50f -> plitresConsumits + 0.0f
+        plitresConsumits in 50f .. 200f -> plitresConsumits * 0.15f
+        else -> plitresConsumits * 0.30f
+    }
 }
